fdfa3014fbc3a90eed618efbe2ef796a5697b0dc
Syed Akber Hasan <158027744+SyedAkberHasan@users.noreply.github.com>
2025-05-31 01:03:36 +0530

    YtscrapperApplication.java

--- YtscrapperApplication.java
+++ YtscrapperApplication.java
@@ -1,6 +1,6 @@
 package com.syedakber.ytscrapper;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
