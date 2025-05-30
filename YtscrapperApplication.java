686ad73b6007539882e257c3c596e829244fe196
Syed Akber Hasan <158027744+SyedAkberHasan@users.noreply.github.com>
2025-05-31 01:00:15 +0530

    Add files via upload

---
+++ YtscrapperApplication.java
@@ -0,0 +1,13 @@
+package com.shivamsharma.ytscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtscrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtscrapperApplication.class, args);
+	}
+
+}
