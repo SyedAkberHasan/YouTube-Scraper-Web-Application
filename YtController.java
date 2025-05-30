6e1daf271ec306f952057296f0492dfaeb6a6bd4
Syed Akber Hasan <158027744+SyedAkberHasan@users.noreply.github.com>
2025-05-31 01:07:34 +0530

    YtController.java

--- YtController.java
+++ YtController.java
@@ -1,4 +1,4 @@
-package com.shivamsharma.ytscrapper.controller;
+package com.syedakber.ytscrapper.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
